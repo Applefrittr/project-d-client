cfe0b291467ec1a13f689708284f0019007ea77d
applefrittr <jacob.lee.jordan@gmail.com>
2025-08-12 20:06:59 -0400

    tweaks to Minion class -> team assignMent, reset, and bounding added

--- src/engine/classes/Minion.ts
+++ src/engine/classes/Minion.ts
@@ -1,24 +1,53 @@
-export default class Minion {
-  x: number;
-  y: number;
+import settings from "../settings.json";
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+export default class Minion {
+  x: number = 0;
+  y: number = 0;
+  dx: number = 0;
+  dy: number = 0;
+  team: "blue" | "red" | null = null;
+
+  assignTeam(team: "red" | "blue") {
+    this.team = team;
+    if (team === "blue") {
+      this.x = 0;
+      this.y = 0;
+      this.dx = 1;
+      this.dy = 1;
+    } else {
+      this.x = settings["arena-width"];
+      this.y = settings["arena-height"];
+      this.dx = -1;
+      this.dy = -1;
+    }
+  }
+
+  reset() {
+    this.team = null;
+    this.x = 0;
+    this.y = 0;
+    this.dx = 0;
+    this.dy = 0;
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath();
-    ctx.fillStyle = "green";
-    ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+    if (typeof this.team === "string") {
+      ctx.beginPath();
+      ctx.fillStyle = this.team;
+      ctx.arc(this.x, this.y, 25, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.closePath();
+    }
   }
 
   update(ctx: CanvasRenderingContext2D | null) {
-    if (ctx) {
-      this.x++;
-      this.y++;
+    // check bounding of Minion, if out of bounds, reset to default settings
+    if (this.x < 0 || this.x > settings["arena-width"]) this.reset();
+
+    // update position coordinates and draw to canvas
+    if (ctx && typeof this.team === "string") {
+      this.x += this.dx;
+      this.y += this.dy;
       this.draw(ctx);
     }
   }
