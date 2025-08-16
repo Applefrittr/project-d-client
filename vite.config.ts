7f26ff8c187d010fc65812541170d272b76ed7d9
applefrittr <jacob.lee.jordan@gmail.com>
2025-08-16 15:49:57 -0400

    gh-pages added and config'd -> dep re-org

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/project-d-client/",
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+    sourcemap: true,
+  },
 });
